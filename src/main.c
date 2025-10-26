65cd12fd4bbab7dab6f829d157b094208b0550d1
rzrn <rzrngh@outlook.com>
2025-10-27 00:55:51 +0700

    src/main.c: “--version” option

--- src/main.c
+++ src/main.c
@@ -787,7 +787,14 @@ int main(int argc, char ** argv) {
             if (argc <= ++i) THROW(-1, "The “--vxl” option requires an argument.\n")
             else vxl_file = argv[i];
         } else MATCH(argv[i], "--help") {
-            THROW(0, "Usage: %s -aos://<ip>:<port> --vxl <file.vxl> --config <file.ini> --team <team> --weapon <weapon> --serverlist <url> --newslist <url>\n", argv[0]);
+            THROW(0, "Usage: %s -aos://<ip>:<port>"
+                     " --vxl <file.vxl> --config <file.ini> --team <team> --weapon <weapon>"
+                     " --serverlist <url> --newslist <url> --offline --help --version\n", argv[0]);
+        } else MATCH(argv[i], "--version") {
+            THROW(0, "TigerSpades %s (%s)\n"
+                     "License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>\n"
+                     "This is free software: you are free to change and redistribute it.\n"
+                     "There is NO WARRANTY, to the extent permitted by law.\n", BSVERSION, GIT_COMMIT_HASH);
         } else MATCH(argv[i], "--serverlist") {
             if (argc <= ++i) THROW(-1, "The “--serverlist” option requires an argument.\n")
             else strnzcpy(serverlist_url, argv[i], sizeof(serverlist_url));

0c9af5bfc4234d7ddf009fc39066e7627141aa4b
rzrn <rzrngh@outlook.com>
2026-03-01 20:11:02 +0700

    build/rules.mk: just in case add ‘.PHONY’ target

--- build/rules.mk
+++ build/rules.mk
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+.PHONY: all clean nuke download game chksum depend printenv
+
 all: $(EXEFILE)
 
 clean:

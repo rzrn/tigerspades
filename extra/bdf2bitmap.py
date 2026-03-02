f63221e986e6e5001b0a761cefe41a00eb57e751
rzrn <rzrngh@outlook.com>
2026-03-02 14:07:35 +0700

    extra/bdf2bitmap.py: clean up

--- extra/bdf2bitmap.py
+++ extra/bdf2bitmap.py
@@ -1,29 +1,51 @@
-from bdfparser import Font, Bitmap
+# Copyright © 2025–2026 rzrn
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import struct
 import sys
 
-_, *files = sys.argv
+from bdfparser import Font, Bitmap
 
-for file in files:
-    font = Font(file)
+progname, *filenames = sys.argv
 
-    print(font.headers)
+for filename in filenames:
+    font = Font(filename)
+
+    headers = ", ".join("{} = {}".format(k, v) for k, v in font.headers.items())
+    print("{}: {}: {}".format(progname, filename, headers))
 
     fontname = font.headers['fontname']
     height = font.headers['fbby']
     high16 = 0x00000000
 
     if height > 255:
-        raise ValueError("too big font")
+        raise ValueError(
+            "{}: {}: too big font".format(progname, filename)
+        )
 
-    fout = open(file + '.out', 'wb')
+    fout = open(filename + '.out', 'wb')
     fout.write(struct.pack("<128sHb", fontname.encode('utf-8'), high16, height))
 
     for glyph in font.iterglyphs():
         codepoint = glyph.cp()
 
         if (codepoint >> 16) != high16:
-            print("skipped codepoint %x" % codepoint)
+            print(
+                "{}: skipped codepoint {:x}".format(progname, codepoint)
+            )
+
             continue
 
         if codepoint > 0x7F:
@@ -32,17 +54,23 @@ for file in files:
         low16 = codepoint & 0xFFFF
 
         bitmap = glyph.draw(2)
+
         if bitmap.height() != height:
-            raise ValueError("bad height glyph (%d)" % codepoint)
+            raise ValueError(
+                "{}: {}: bad glyph height: {:x}".format(progname, filename, codepoint)
+            )
 
         if bitmap.width() > 255:
-            raise ValueError("too big glyph (%d)" % codepoint)
+            raise ValueError(
+                "{}: {}: glyph too big: {:x}".format(progname, filename, codepoint)
+            )
 
         if bitmap.width() % 8 != 0:
-            raise ValueError("invalid glyph width (%d)" % codepoint)
+            raise ValueError(
+                "{}: {}: invalid glyph width: {:x}".format(progname, filename, codepoint)
+            )
 
         data = bitmap.tobytes('1', {0: 0, 1: 1, 2: 1})
         fout.write(struct.pack("<Hb", low16, bitmap.width() // 8) + data)
 
     fout.close()
-
